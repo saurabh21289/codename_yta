20a9d65aa6ceee936287ee000bcc194d4641693f
Saurabh Singh <saurabh.singh@digi.com>
2018-04-06 19:43:59 -0400

    Basic library test, updated requirements.txt
    Signed-off-by: Saurabh Singh <saurabh.singh@digi.com>

--- app.py
+++ app.py
@@ -1,1 +1,5 @@
 import imageio
+from google_images_download import google_images_download
+
+response = google_images_download.googleimagesdownload()
+response.download({"keywords": "beijing pollution", "limit": 5, "size": "large"})
